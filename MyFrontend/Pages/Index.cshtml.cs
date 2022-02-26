026b28c2d70cc3081ef712f363e3b76b490dd66b
Marcel Körtgen <marcel.koertgen@gmail.com>
2022-02-26 02:57:57 +0100

    Initial commit

---
+++ MyFrontend/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFrontend.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly DaprClient _daprClient;
+
+    public IndexModel(DaprClient daprClient)
+    {
+        _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
+    }
+
+    public async Task OnGetAsync()
+    {
+        var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+            HttpMethod.Get,
+            "MyBackEnd",
+            "weatherforecast");
+
+        ViewData["WeatherForecastData"] = forecasts;
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# hello.dapr
+
+## Getting Started
+
+Having [dapr-cli installed](https://docs.dapr.io/getting-started/install-dapr-cli/)
+
+```shell
+$ dapr init
+```
+
+## References
+
+See
+
+- [Get started with Dapr](https://docs.microsoft.com/en-us/dotnet/architecture/dapr-for-net-developers/getting-started)
